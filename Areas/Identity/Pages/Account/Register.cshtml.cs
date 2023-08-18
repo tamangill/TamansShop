3786f8a7f6341529ec3ee0346a71a2f3f559b8c5
Taman Gill <tgill0424@gmail.com>
2023-08-18 13:08:58 -0700

    basic registration dome

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using TamansShop.Areas.Identity.Data;
 
 namespace TamansShop.Areas.Identity.Pages.Account
@@ -71,6 +72,19 @@ namespace TamansShop.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+
+
+            [Required]
+            [StringLength(255)]
+            [Display(Name = "FirstName")]
+            public string FirstName { get; set; }
+            
+            [Required]
+            [StringLength(255)]
+            [Display(Name = "LastName")]
+            public string LastName { get; set; }
+
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,6 +128,9 @@ namespace TamansShop.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
